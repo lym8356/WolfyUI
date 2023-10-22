a53f4ce6e55e47cdb7c7006da1993045f9389fa7
Adam Lu <lym8356@gmail.com>
2023-10-22 19:13:53 +1100

    Default styles presets

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
+      <h2>Vite + React</h2>
+      <h3>Vite + React</h3>
+      <hr />
+      <code>
+        const a = 'b';
+      </code>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
